500b5bd283d936c967ac3547462a21e40a408859
eletronicastro <106507526+eletronicastro@users.noreply.github.com>
2022-05-30 02:28:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("button").addEventListener("click", function (){
+    document.querySelector(".popup").style.display = "flex";
+ })
+
+ document.querySelector(".close").addEventListener("click", function (){
+    document.querySelector(".popup").style.display = "none";
+})
+
+document.getElementById("button2").addEventListener("click", function (){
+    document.querySelector(".popup2").style.display = "flex";
+ })
+
+ document.querySelector(".close").addEventListener("click", function (){
+    document.querySelector(".popup2").style.display = "none";
+})
